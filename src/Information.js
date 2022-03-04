45b041988f296afa3692cde8314d391a03430f90
kouhei-sugahara <200092tw@yse-c.net>
2022-03-04 14:32:16 +0900

    firstcommit(final)

---
+++ src/Information.js
@@ -0,0 +1,29 @@
+import React, {state, useState} from 'react'
+import PropTypes from 'prop-types'
+
+function Information(props) {
+    const [count, setCount] = useState(0)
+
+    function hello(){
+        //alert('hello')
+        setCount(count + 1)
+    }
+    return (
+        //<div>Information</div>
+        <div>
+            <button onClick={hello}>Hello</button>
+            <p>{count}</p>
+            <ul>
+                <li>新商品追加！</li>
+                <li>本日は休業日です</li>
+            </ul>
+        </div>
+    )
+}
+
+Information.propTypes = {
+
+}
+
+export default Information
+
